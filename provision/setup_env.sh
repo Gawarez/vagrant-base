25e3922524d45eb93fbaaeb00e2ed8159d658a20
Gawarez <gabrieltbaggio@gmail.com>
2024-10-23 14:22:00 -0300

    provision: install zip

--- provision/setup_env.sh
+++ provision/setup_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y ranger ncdu bash-completion sd
+sudo apt install -y ranger ncdu bash-completion sd zip
 git clone --depth 1 https://github.com/junegunn/fzf.git /home/vagrant/.fzf
 yes | /home/vagrant/.fzf/install
