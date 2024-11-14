ec2395a3cc3452123aadd85a37761ab61b43b39a
Gawarez <gabrieltbaggio@gmail.com>
2024-11-14 15:50:00 -0300

    todo: helm bash completion

--- provision/kubernetes.sh
+++ provision/kubernetes.sh
@@ -8,6 +8,8 @@ echo "export KUBECONFIG=~/.kube/config" >> ~/.bashrc
 
 # Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+# TODO: add bash completion to helm
+# helm completion bash > /etc/bash_completion.d/helm
 
 # K9s
 curl -o k9s.tar.gz -L https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_amd64.tar.gz
